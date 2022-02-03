ec3fda58c42b113a9fbd683ae43c23d35d93422e
Stephen Tedesco <s_tylert@icloud.com>
2022-02-02 18:36:00 -0700

    Create First Three Functions

--- src/book.js
+++ src/book.js
@@ -1,11 +1,34 @@
-function createTitle() {
-  
+function createTitle(title) {
+
+  return `The ${title}`;
 }
+
+function buildMainCharacter(name, age, pronouns) {
+
+  class character {
+    constructor(){
+      this.name = name,
+      this.age = age,
+      this.pronouns = pronouns
+    }
+  };
+
+var mainChar = new character();
+return mainChar;
+};
+
+
+function saveReview(str, reviews) {
+
+  reviews.push(str);
+
+  return reviews;
+};
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
+  buildMainCharacter,
+  saveReview,
   // calculatePageCount,
   // writeBook,
   // editBook
