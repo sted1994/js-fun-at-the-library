b0eacff445e5a4622faecca16488b21deed2ce57
Stephen Tedesco <s_tylert@icloud.com>
2022-02-03 18:54:07 -0700

    Complete shelfBook function

--- src/shelf.js
+++ src/shelf.js
@@ -1,6 +1,30 @@
 
+function shelfBook(book, sciFiShelf) {
+
+
+   if ((book.genre === "sciFi") && (sciFiShelf.length < 3)) {
+     sciFiShelf.unshift(book);
+   }
+
+}
+
+function unshelfBook() {
+
+
+}
+
+function listTitles() {
+
+
+}
+
+function searchShelf() {
+
+
+}
+
 module.exports = {
-  // shelfBook,
+  shelfBook,
   // unshelfBook,
   // listTitles,
   // searchShelf
