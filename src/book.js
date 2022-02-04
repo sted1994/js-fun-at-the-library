c71c48aa66fc778e24c96cc3ede5219d178fc7f1
Stephen Tedesco <s_tylert@icloud.com>
2022-02-03 17:55:46 -0700

    Complete Functions

--- src/book.js
+++ src/book.js
@@ -17,19 +17,42 @@ var mainChar = new character();
 return mainChar;
 };
 
-
 function saveReview(str, reviews) {
 
-  reviews.push(str);
+  if (reviews.includes(str) === false) {
+    reviews.push(str);
+  }
 
   return reviews;
+};
+
+function calculatePageCount(title) {
+  return title.length * 20;
+};
+
+function writeBook(bookTitle, bookCharacter, genre) {
+
+
+var book = {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    pageCount: calculatePageCount(bookTitle),
+    genre: genre,
+  };
+return book;
+};
+
+function editBook(title) {
+
+  title.pageCount = (title.pageCount * .75);
+
 };
 
 module.exports = {
   createTitle,
   buildMainCharacter,
   saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  calculatePageCount,
+  writeBook,
+  editBook
 }
